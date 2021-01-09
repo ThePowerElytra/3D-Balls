2d59f124b210b76447e0e669415b470ce32dd5c6
Toby Foster <tobfos26111@gmail.com>
2021-01-09 21:51:36 +1100

    I don't know why the numbers variable isn't working bu I'll deal with it later

--- variables.py
+++ variables.py
@@ -1,7 +1,11 @@
 me_name = "Toby Foster"
 was_here = "wasn't"
+was_here_q = False
 print(""+ me_name +" "+ was_here +" here")
 
 me_name = "tobfos"
 was_here ="was"
+was_here_q = True
+numbers = 1234567890.123456789
 print(""+ me_name +" "+ was_here +" here")
+print("+ numbers +")
