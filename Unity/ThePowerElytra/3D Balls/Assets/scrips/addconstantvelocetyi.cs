16a469da1e0d87a26b913af465994f00df49ea54
Blake Foster <fosterblake2008@gmail.com>
2021-01-21 16:24:34 +1100

    player control and movement

---
+++ Unity/ThePowerElytra/3D Balls/Assets/scrips/addconstantvelocetyi.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addconstantvelocetyi : MonoBehaviour
+{
+
+    [SerializeField]
+    Vector3 v3Force;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity += new Vector3(0.1f, 0, 0);
+    }
+}
---
+++ Unity/ThePowerElytra/3D Balls/Assets/scrips/addplayercontrol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addplayercontrol : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+    [SerializeField]
+    KeyCode KeyPositive;
+    [SerializeField]
+    KeyCode KeyNegative;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyPositive))
+        GetComponent<Rigidbody>().velocity += v3Force;
+        
+        if (Input.GetKey(KeyNegative))
+        GetComponent<Rigidbody>().velocity -= v3Force;
+    }
+}
